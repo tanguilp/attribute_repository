de584501bf95bfb8ac91c09ad37e1434efc272ce
Tangui <29804907+tanguilp@users.noreply.github.com>
2019-02-15 20:27:19 +0300

    Initial commit

---
+++ lib/attribute_repository.ex
@@ -0,0 +1,18 @@
+defmodule AttributeRepository do
+  @moduledoc """
+  Documentation for AttributeRepository.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AttributeRepository.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/attribute_repository_test.exs
@@ -0,0 +1,8 @@
+defmodule AttributeRepositoryTest do
+  use ExUnit.Case
+  doctest AttributeRepository
+
+  test "greets the world" do
+    assert AttributeRepository.hello() == :world
+  end
+end
