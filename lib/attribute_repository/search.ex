15fae1189ec7477f4ec648a995c3343c5872e0e2
Tangui <29804907+tanguilp@users.noreply.github.com>
2019-03-01 21:48:59 +0300

    Cleaning search module

--- lib/attribute_repository/search.ex
+++ lib/attribute_repository/search.ex
@@ -8,16 +8,4 @@ defmodule AttributeRepository.Search do
     AttributeRepository.run_opts()
   ) :: {:ok, [AttributeRepository.resource()]}
   | {:error, %AttributeRepository.ReadError{}}
-
-  defmodule Filter do
-    @type t :: any()
-  end
-
-  defmodule NotFoundError do
-    @moduledoc """
-    Error returned when a resource expected to be found was not found
-    """
-
-    defexception message: "Resource not found"
-  end
 end
